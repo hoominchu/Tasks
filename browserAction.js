31065fbac1e11390b3053859c0ab4c550c9895ec
hoominchu <hoominchu@gmail.com>
2017-11-18 19:07:14 +0530

    temp commit

--- browserAction.js
+++ browserAction.js
@@ -8,60 +8,88 @@
 
 var TASKS;
 
-chrome.storage.local.get("TASKS", function(taskObject){
-  if(taskObject["TASKS"]){
-    TASKS = taskObject["TASKS"];  //On retreiving TASKS from chrome storage, one gets an object {TASKS: balhah}, to retreive the actual array call taskObject["TASKS"]
-    for(var task_id in TASKS){
-      if(task_id != "lastAssignedId"){
-        var li = document.createElement("li");
-        var cancelButton = document.createElement("p");
-        cancelButton.innerText = "Delete";
-        cancelButton.className = "delete"
-        li.innerText = TASKS[task_id].name;
-        li.id = TASKS[task_id].id;
-        li.className = "task";
-        li.appendChild(cancelButton);
-        document.getElementById("tasks").appendChild(li);
-      }
-    }
-    for(var i =0; i<document.getElementsByClassName("task").length; i++){
-      document.getElementsByClassName("task")[i].addEventListener("click", function(task){
-        return function(task){
-          chrome.runtime.sendMessage(
-            {
-              "type": "switch-task",
-              "nextTaskId": task.srcElement.id
+chrome.storage.local.get("TASKS", function (taskObject) {
+    if (taskObject["TASKS"]) {
+        TASKS = taskObject["TASKS"];  //On retreiving TASKS from chrome storage, one gets an object {TASKS: balhah}, to retreive the actual array call taskObject["TASKS"]
+        for (var task_id in TASKS) {
+            if (task_id != "lastAssignedId") {
+                var colElement = document.createElement("col-lg-2");
+                var card = document.createElement("div");
+                var cardBody = document.createElement("div");
+                var cardTaskName = document.createElement("p");
+                var buttonGroup = document.createElement("div");
+                var openTaskButton = document.createElement("button");
+                card.className = "card border-dark round-corner";
+                cardBody.className = "card-body text-center";
+                cardTaskName.innerText = TASKS[task_id].name;
+                buttonGroup.className = "btn-group";
+                openTaskButton.className = "btn btn-outline-primary round-corner task";
+                openTaskButton.type = "button";
+                openTaskButton.innerText = "Open Task";
+                openTaskButton.id = TASKS[task_id].id;
+                colElement.appendChild(card);
+                card.appendChild(cardBody);
+                cardBody.appendChild(cardTaskName);
+                buttonGroup.appendChild(openTaskButton);
+                card.appendChild(buttonGroup);
+                // buttonGroup.style = "margin-bottom : 0.2em";
+
+                document.getElementById("tasks").appendChild(colElement);
+
+                // var openTaskButton = document.createElement("li");
+                // openTaskButton.className = "btn btn-outline-primary round-corner task";
+                // openTaskButton.type = "button";
+                // openTaskButton.innerText = "Open Task";
+                // openTaskButton.id = TASKS[task_id].id;
+                // document.getElementById("tasks").appendChild(openTaskButton);
+
+
+                // var li = document.createElement("li");
+                // var cancelButton = document.createElement("p");
+                // cancelButton.innerText = "Delete";
+                // cancelButton.className = "delete"
+                // li.innerText = TASKS[task_id].name;
+                // li.id = TASKS[task_id].id;
+                // li.className = "task";
+                // li.appendChild(cancelButton);
+                // document.getElementById("tasks").appendChild(li);
             }
-          );
-        }(task);
-      });
-      document.getElementsByClassName("delete")[i].addEventListener("click", function(deleteButton){
-        return function(deleteButton){
-          chrome.runtime.sendMessage(
-            {
-              "type": "delete-task",
-              "taskToRemove": deleteButton.srcElement.parentElement.id
-            }
-          );
-        }(deleteButton);
-      });
+        }
+        for (var i = 0; i < document.getElementsByClassName("task").length; i++) {
+            document.getElementsByClassName("task")[i].addEventListener("click", function (task) {
+                return function (task) {
+                    chrome.runtime.sendMessage(
+                        {
+                            "type": "switch-task",
+                            "nextTaskId": task.srcElement.id
+                        }
+                    );
+                }(task);
+            });
+            // document.getElementsByClassName("delete")[i].addEventListener("click", function (deleteButton) {
+            //     return function (deleteButton) {
+            //         chrome.runtime.sendMessage(
+            //             {
+            //                 "type": "delete-task",
+            //                 "taskToRemove": deleteButton.srcElement.parentElement.id
+            //             }
+            //         );
+            //     }(deleteButton);
+            // });
+        }
     }
-  }
 });
 
 
-
-
-
-document.getElementById("createTask").addEventListener("click", function(){
-  chrome.runtime.sendMessage(
-    {
-      "type": "create-task",
-      "taskName": document.getElementById("taskName").value,
-      "tabs": [],
-      "activated": true
-    }
-  );
+document.getElementById("createTask").addEventListener("click", function () {
+    chrome.runtime.sendMessage(
+        {
+            "type": "create-task",
+            "taskName": document.getElementById("taskName").value,
+            "tabs": [],
+            "activated": true
+        }
+    );
 });
 
 // chrome.getElementById("work").addEventListener("click", function(){
--- getTabs.js
+++ getTabs.js
@@ -2,23 +2,23 @@
 
 //Set up TASKS array. Set it to empty if it doesn't exist in chrome storage, otherwise retreive it from chrome storage.
 var TASKS = {
-  lastAssignedId: -1
-}
+    lastAssignedId: -1
+};
 
 var CTASKID = -1;
 
 //Initialising TASKS and CTASKID
-chrome.storage.local.get("TASKS", function(taskObject){
-  if(taskObject["TASKS"]){
-    TASKS = taskObject["TASKS"];  //On retreiving TASKS from chrome storage, one gets an object {TASKS: balhah}, to retreive the actual array call taskObject["TASKS"]
-  }
+chrome.storage.local.get("TASKS", function (taskObject) {
+    if (taskObject["TASKS"]) {
+        TASKS = taskObject["TASKS"];  //On retreiving TASKS from chrome storage, one gets an object {TASKS: balhah}, to retreive the actual array call taskObject["TASKS"]
+    }
 });
 
-chrome.storage.local.get("CTASKID", function(cTaskIdObject){
-  if(cTaskIdObject["CTASKID"]){
-    CTASKID = cTaskIdObject["CTASKID"];
-  }
-})
+chrome.storage.local.get("CTASKID", function (cTaskIdObject) {
+    if (cTaskIdObject["CTASKID"]) {
+        CTASKID = cTaskIdObject["CTASKID"];
+    }
+});
 
 function Task(task_id, task_name, tabs) {
     this.id = task_id;
@@ -28,95 +28,95 @@ function Task(task_id, task_name, tabs) {
 
 //Helper Methods
 
-function createTask(taskName, tabs){
-  var newTask = new Task(TASKS["lastAssignedId"]+1, taskName, tabs);
-  TASKS[TASKS["lastAssignedId"]+1] = newTask;
-  TASKS["lastAssignedId"] = TASKS["lastAssignedId"] + 1;
-  chrome.storage.local.set({"TASKS": TASKS});
-}
-
-function activateTask(task_id){
-  try {
-    if(TASKS[task_id].tabs.length>0){
-      for(var i = 0; i<TASKS[task_id].tabs.length; i++){
-        chrome.tabs.create({"url": TASKS[task_id].tabs[i].url});
-      }
-    }
-    else{
-      chrome.tabs.create({"url": "about:blank"});
-    }
-
-    CTASKID = task_id;
-    chrome.storage.local.set({"CTASKID": task_id});
-  }
-  catch(err){
-    console.log(err.message);
-  }
-}
-
-function saveTask(task_id){
-  if(TASKS[task_id]){
-    chrome.tabs.query({}, function(allTabs){
-      TASKS[task_id].tabs = allTabs;
-      chrome.storage.local.set({"TASKS": TASKS});
-    });
-  }
-}
-
-function deactivateTask(currentTaskId){
-
-  saveTask(currentTaskId);
-
-  chrome.tabs.query({}, function(allTabs){
-    for(var i = 0; i<allTabs.length; i++){
-      chrome.tabs.remove(allTabs[i].id);
-    }
-  });
-}
-
-function deleteTask(task_id){
-  if(TASKS[task_id]){
-    delete TASKS[task_id];
+function createTask(taskName, tabs) {
+    var newTask = new Task(TASKS["lastAssignedId"] + 1, taskName, tabs);
+    TASKS[TASKS["lastAssignedId"] + 1] = newTask;
+    TASKS["lastAssignedId"] = TASKS["lastAssignedId"] + 1;
     chrome.storage.local.set({"TASKS": TASKS});
-  }
+}
+
+function activateTask(task_id) {
+    try {
+        if (TASKS[task_id].tabs.length > 0) {
+            for (var i = 0; i < TASKS[task_id].tabs.length; i++) {
+                chrome.tabs.create({"url": TASKS[task_id].tabs[i].url});
+            }
+        }
+        else {
+            chrome.tabs.create({"url": "about:blank"});
+        }
+
+        CTASKID = task_id;
+        chrome.storage.local.set({"CTASKID": task_id});
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+}
+
+function saveTask(task_id) {
+    if (TASKS[task_id]) {
+        chrome.tabs.query({}, function (allTabs) {
+            TASKS[task_id].tabs = allTabs;
+            chrome.storage.local.set({"TASKS": TASKS});
+        });
+    }
+}
+
+function deactivateTask(currentTaskId) {
+
+    saveTask(currentTaskId);
+
+    chrome.tabs.query({}, function (allTabs) {
+        for (var i = 0; i < allTabs.length; i++) {
+            chrome.tabs.remove(allTabs[i].id);
+        }
+    });
+}
+
+function deleteTask(task_id) {
+    if (TASKS[task_id]) {
+        delete TASKS[task_id];
+        chrome.storage.local.set({"TASKS": TASKS});
+    }
 }
 
 //Saving Events
-chrome.runtime.onMessage.addListener(function(request, sender){
-  console.log(request);
+chrome.runtime.onMessage.addListener(function (request, sender) {
+    console.log(request);
 
-  if(request.type == "create-task"){
+    if (request.type == "create-task") {
 
-    createTask(request.taskName, request.tabs);
+        createTask(request.taskName, request.tabs);
 
-    if(request.activated){
-      deactivateTask(CTASKID);
-      activateTask(TASKS["lastAssignedId"]);
+        if (request.activated) {
+            deactivateTask(CTASKID);
+            activateTask(TASKS["lastAssignedId"]);
+        }
     }
-  }
 
-  if(request.type == "switch-task" && request.nextTaskId!=""){
-    deactivateTask(CTASKID);
-    activateTask(request.nextTaskId);
-  }
+    if (request.type == "switch-task" && request.nextTaskId != "") {
+        deactivateTask(CTASKID);
+        activateTask(request.nextTaskId);
+    }
 
-  if(request.type == "delete-task"){
-    deleteTask(request.taskToRemove);
-  }
+    if (request.type == "delete-task") {
+        deleteTask(request.taskToRemove);
+    }
 
 
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-      if(changeInfo.title){
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.title) {
         saveTask(CTASKID);
-      }
+    }
 });
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-      if(!removeInfo.isWindowClosing){
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    if (!removeInfo.isWindowClosing) {
         saveTask(CTASKID);
-      }
+    }
 });
 
 
