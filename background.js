a488f84cdbf03dcfc9aef120116bc45436a63169
hoominchu <hoominchu@gmail.com>
2018-02-20 12:43:50 +0530

    Selecting and getting author from a webpage (if the selectors of that website are mentioned in dictionary and log frequencies in the chrome.storage.local

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// Setting up required variables. These should be used as they are across files.
+// Local storage fields
+var preferredAuthorsFieldName = "Preferred authors";
+var totalFrequencyFieldName = "Total frequency";
 
 //BEGINNING: Object Definitions
 
@@ -33,6 +37,16 @@ chrome.storage.local.get("TASKS", function (taskObject) {
 chrome.storage.local.get("CTASKID", function (cTaskIdObject) {
     if (cTaskIdObject["CTASKID"]) {
         CTASKID = cTaskIdObject["CTASKID"];
+    }
+});
+
+chrome.storage.local.get(preferredAuthorsFieldName, function (prefAuthObj) {
+    if (prefAuthObj[preferredAuthorsFieldName]) {
+        // Adding to the local storage if the field doesn't exist already.
+        var o = {};
+        o[preferredAuthorsFieldName] = {};
+        o["metadata"][totalFrequencyFieldName] = 0;
+        chrome.storage.local.set(o);
     }
 });
 
@@ -273,8 +287,11 @@ function refreshContextMenu() {
                 });
             }
         }
+        chrome.contextMenus.create({"title": "Save to Sailboat", "id": "saveToSailboat", "contexts": ["link"]});
+
     })
 }
+
 
 function addToTask(url, task_id) {
     TASKS[task_id].tabs.push({"url": url});
@@ -283,6 +300,13 @@ function addToTask(url, task_id) {
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.parentMenuItemId == "rootMenu") {
         addToTask(info.linkUrl, info.menuItemId);
+    }
+    else if (info.parentMenuItemId == "saveToSailboat") {
+        var currentTaskName = TASKS[CTASKID].name;
+        var pathToFolder; //drive + "Sailboat/" + currentTaskName;
+        console.log("context menu clicked");
+        console.log(info.linkUrl);
+        saveFileToDrive(info.linkUrl);
     }
 });
 
@@ -361,57 +385,7 @@ chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
 
 //ENDING: Stuff that saves tabs of task
 
-// OAuth
 
-function revokeToken() {
-
-    chrome.identity.getAuthToken({ 'interactive': false },
-        function(current_token) {
-            if (!chrome.runtime.lastError) {
-
-                // @corecode_begin removeAndRevokeAuthToken
-                // @corecode_begin removeCachedAuthToken
-                // Remove the local cached token
-                chrome.identity.removeCachedAuthToken({ token: current_token },
-                    function() {});
-                // @corecode_end removeCachedAuthToken
-
-                // Make a request to revoke token in the server
-                var xhr = new XMLHttpRequest();
-                xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' +
-                    current_token);
-                xhr.send();
-                // @corecode_end removeAndRevokeAuthToken
-
-                // Update the user interface accordingly
-
-                $('#revoke').get(0).disabled = true;
-                console.log('Token revoked and removed from cache. '+
-                    'Check chrome://identity-internals to confirm.');
-            }
-        });
-}
-
-document.getElementById('login').addEventListener("click", function () {
-    chrome.identity.getAuthToken({
-        interactive: true
-    }, function (token) {
-        if (chrome.runtime.lastError) {
-            alert(chrome.runtime.lastError.message);
-            return;
-        }
-        var x = new XMLHttpRequest();
-        x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
-        x.onload = function () {
-            alert(x.response);
-        };
-        x.send();
-    });
-});
-
-document.getElementById('logout').addEventListener("click", function () {
-    revokeToken();
-});
 
 
 
