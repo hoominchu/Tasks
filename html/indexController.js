78e7a5f6b95e8f77b96e2218741fdca56ce4bf2a
Kshitij Kapoor <kdev@Kshitijs-Macbook.local>
2018-02-28 15:44:12 +0530

    All the buttons on index are operational

--- html/indexController.js
+++ html/indexController.js
@@ -1,50 +1,112 @@
 window.onload = function () {
-  chrome.storage.local.get("TASKS", function (taskObject) {
-    if(taskObject["TASKS"]){
-      console.log(taskObject["TASKS"]);
-      showTasks(taskObject["TASKS"]);
-      for (var i = 0; i < document.getElementsByClassName("task").length; i++) {
-          console.log("hello");
-          document.getElementsByClassName("task")[i].addEventListener("click", function (task) {
-              console.log($(task).closest(".card").attr("id"));
-              return function (task) {
-                  chrome.runtime.sendMessage(
-                      {
-                          "type": "switch-task",
-                          "nextTaskId": $(task.srcElement).closest(".card").attr("id")
-                      }
-                  );
-              }(task);
-          });
+
+    chrome.storage.local.get("TASKS", function (taskObject) {
+
+        if (taskObject["TASKS"]) {
+
+
+
+            showTasks(taskObject["TASKS"]);
+
+            funcOnClick("openTask", "class", function (element) {
+                return function (element) {
+                    chrome.runtime.sendMessage(
+                        {
+                            "type": "switch-task",
+                            "nextTaskId": $(element.srcElement).closest(".card").attr("id")
+                        }
+                    );
+                }(element);
+            });
+
+            funcOnClick("deleteTask", "class", function (element) {
+                var Tasks = taskObject["TASKS"];
+                if(Tasks[$(element.srcElement).closest(".card").attr("id")].isActive){
+                    alert("Task is active. Can't delete.");
+                }
+                else{
+                    return function (element) {
+                        chrome.runtime.sendMessage(
+                            {
+                                "type": "delete-task",
+                                "taskToRemove": $(element.srcElement).closest(".card").attr("id")
+                            }
+                        );
+                        location.reload();
+                    }(element);
+                }
+
+            });
+
+            funcOnClick("renameTask", "class", function (element) {
+
+                var renameElement = element;
+
+                $('#renameModal').modal('show');
+
+                funcOnClick("renameSubmit", "id", function (e) {
+                    return function (element) {
+                        chrome.runtime.sendMessage(
+                            {
+                                "type": "rename-task",
+                                "taskId": $(renameElement.srcElement).closest(".card").attr("id"),
+                                "newTaskName": $("#newNameForTask").val()
+                            }
+                        );
+                        location.reload();
+
+                    }(e);
+
+                });
+            })
+
         }
+    });
 
-    }
-  });
 
-  document.getElementById("createTask").addEventListener("click", function () {
-    sendCreateTaskMessage();
-  });
 };
 
-function sendCreateTaskMessage(){
-  chrome.tabs.query({}, function(tabs){
-    chrome.bookmarks.getTree(function(bookmarks){
-      chrome.runtime.sendMessage(
-          {
-              "type": "create-task",
-              "taskName": document.getElementById("taskName").value,
-              "createFromCurrentTabs": document.getElementById("createFromCurrentTabs").checked,
-              "tabs": tabs,
-              "bookmarks": bookmarks,
-              "activated": true
-          }
-      );
+document.getElementById("createTask").addEventListener("click", function(){
+    sendCreateTaskMessage();
+});
+
+
+function sendCreateTaskMessage() {
+    chrome.tabs.query({}, function (tabs) {
+        chrome.bookmarks.getTree(function (bookmarks) {
+            chrome.runtime.sendMessage(
+                {
+                    "type": "create-task",
+                    "taskName": document.getElementById("taskName").value,
+                    "createFromCurrentTabs": document.getElementById("createFromCurrentTabs").checked,
+                    "tabs": tabs,
+                    "bookmarks": bookmarks,
+                    "activated": true
+                }
+            );
+        });
     });
-  });
 }
 
-$("#downloadTasks").click(function(){
-  chrome.runtime.sendMessage({
-    "type": "download-tasks"
-  });
+function funcOnClick(classNameOrIdName, type, func) {
+    if (type == "class") {
+        for (var i = 0; i < document.getElementsByClassName(classNameOrIdName).length; i++) {
+            document.getElementsByClassName(classNameOrIdName)[i].addEventListener("click", function (element) {
+                func(element);
+            });
+        }
+    }
+    if (type == "id") {
+        document.getElementById(classNameOrIdName).addEventListener("click", function (element) {
+            func(element);
+        });
+    }
+
+}
+
+
+$("#downloadTasks").click(function () {
+    chrome.runtime.sendMessage({
+        "type": "download-tasks"
+    });
 });
--- html/indexView.js
+++ html/indexView.js
@@ -1,19 +1,19 @@
 function showTasks(Tasks) {
     for (var task_id in Tasks) {
-        if (task_id != "lastAssignedId") {
+        if (task_id != "lastAssignedId" && Tasks[task_id].id != 0) {
 
             var col = $("<div class='col-lg-3'></div>");
 
             if (Tasks[task_id].isActive) {
                 var card = $("<div>", {
-                    "class": "card border-dark mb-3 task",
+                    "class": "card border-primary mb-3 task",
                     "style": "max-width: 20rem; height:12em; border-radius:0.8em",
                     "id": task_id
                 });
             }
             else {
                 var card = $("<div>", {
-                    "class": "card border-dark mb-3 task",
+                    "class": "card border-secondary mb-3 task",
                     "style": "max-width: 20rem; height:12em; border-radius:0.8em",
                     "id": task_id
                 });
@@ -22,16 +22,13 @@ function showTasks(Tasks) {
             var card_header = $("<div>", {"class": "card-header", "text": Tasks[task_id].name});
             var card_body = $("<div>", {"class": "card-body text-dark"});
             var card_buttons = $("<div>", {"class": "btn-group"});
-            var open_button = $("<button class='btn btn-outline-success btn-sm round-corner-left' type='button' id='" + Tasks[task_id].id + "'>Open</button>");
-            var delete_button = $("<button class='btn btn-outline-danger btn-sm round-corner-left' type='button' id='" + Tasks[task_id].id + "'>Delete</button>");
-            var rename_button = $("<button class='btn btn-outline-dark btn-sm round-corner' type='button' id='" + Tasks[task_id].id + "'>Rename</button>");
-            var share_button = $("<button class='btn btn-outline-primary btn-sm round-corner-right' type='button' id='" + Tasks[task_id].id + "'>Share</button>");
-            var archive_button = $("<button class='btn btn-outline-primary btn-sm round-corner-right' type='button' id='" + Tasks[task_id].id + "'>Archive</button>");
+            var open_button = $("<button class='btn btn-outline-success btn-sm round-corner-left openTask' type='button' id='" + Tasks[task_id].id + "'>Open</button>");
+            var rename_button = $("<button class='btn btn-outline-dark btn-sm round-corner renameTask' type='button' id='" + Tasks[task_id].id + "'>Rename</button>");
+            var delete_button = $("<button class='btn btn-outline-danger btn-sm round-corner-right deleteTask' type='button' id='" + Tasks[task_id].id + "'>Delete</button>");
+
             card_buttons.append(open_button);
             card_buttons.append(rename_button);
-            card_buttons.append(share_button);
             card_buttons.append(delete_button);
-            card_buttons.append(archive_button);
             var card_thumbnail = $("<h1 class='text-center' style='font-size:500%; font-weight:bold'>" + Tasks[task_id].name.slice(0, 4) + "</h1>");
             //var card_toolbar = $("<div>")
             //var renameButton = $("<button>", {"text": "Rename", "class":"btn rename round-corner", "type": "button", "id": "rename_"+Tasks[task_id].id});
