48b7d5b455135e3f5f01492c645419416bc0f7ab
hoominchu <hoominchu@gmail.com>
2018-03-21 19:52:01 +0530

    Suggestions based on liked pages.

--- html/statsView.js
+++ html/statsView.js
@@ -1,29 +1,31 @@
-function showStats(tasks, ctaskid){
-  console.log(ctaskid)
-  ctaskidString = ctaskid.toString();
-  $("#currentTask").text(tasks[ctaskidString].name);
+function showStats(tasks, ctaskid) {
+    console.log(ctaskid);
+    ctaskidString = ctaskid.toString();
+    $("#currentTask").text(tasks[ctaskidString].name);
 
-  function showTime(){
-    var lastActivation = tasks[ctaskidString].activationTime[tasks[ctaskidString].activationTime.length-1];
-    var currentTime = new Date();
-    function returnDuration(endingTime, startingTime){
-      var startingTime = new Date(startingTime);
-      var endingTime = new Date(endingTime);
-      var hours = Math.abs(endingTime.getHours() - startingTime.getHours());
-      var minutes = Math.abs(endingTime.getMinutes() - startingTime.getMinutes());
-      var seconds = Math.abs(endingTime.getSeconds() - startingTime.getSeconds());
-      var duration = {
-        "hours": hours,
-        "minutes": minutes,
-        "seconds": seconds
-      }
-      return duration;
+    function showTime() {
+        var lastActivation = tasks[ctaskidString].activationTime[tasks[ctaskidString].activationTime.length - 1];
+        var currentTime = new Date();
+
+        function returnDuration(endingTime, startingTime) {
+            var startingTime = new Date(startingTime);
+            var endingTime = new Date(endingTime);
+            var hours = Math.abs(endingTime.getHours() - startingTime.getHours());
+            var minutes = Math.abs(endingTime.getMinutes() - startingTime.getMinutes());
+            var seconds = Math.abs(endingTime.getSeconds() - startingTime.getSeconds());
+            var duration = {
+                "hours": hours,
+                "minutes": minutes,
+                "seconds": seconds
+            };
+            return duration;
+        }
+
+        $("#time-hours").text(returnDuration(currentTime, lastActivation).hours + " hours");
+        $("#time-minutes").text(returnDuration(currentTime, lastActivation).minutes + " minutes");
+        $("#time-seconds").text(returnDuration(currentTime, lastActivation).seconds + " seconds");
     }
-    $("#time-hours").text(returnDuration(currentTime, lastActivation).hours + " hours");
-    $("#time-minutes").text(returnDuration(currentTime, lastActivation).minutes + " minutes");
-    $("#time-seconds").text(returnDuration(currentTime, lastActivation).seconds + " seconds");
-  }
 
-  setInterval(showTime, 1000);
+    setInterval(showTime, 1000);
 
 }
