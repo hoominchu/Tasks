796c9bad3fff842a649bc5462827e1311decb1ca
Kshitij Kapoor <kdev@Kshitijs-Macbook.local>
2018-04-26 14:51:47 +0530

    Fixed bug in text log and page content removal.

--- background.js
+++ background.js
@@ -126,8 +126,8 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
 chrome.windows.onRemoved.addListener(function(windowId){
     if(windowId != backgroundPageId){
         //deactivateTaskInWindow(getKeyByValue(taskToWindow, windowId));
-        console.log("Window Removed" + TASKS);
-        console.log(TASKS);
+        //console.log("Window Removed" + TASKS);
+        //console.log(TASKS);
         delete taskToWindow[getKeyByValue(taskToWindow, windowId)];
         // getIdsOfCurrentlyOpenTabs(windowId, function(ids){console.log(ids)});
     }
@@ -213,7 +213,7 @@ chrome.windows.onFocusChanged.addListener(function (newWindowId){
         removeFromPageContentAndTextLog(url);
       }
     }
-  })
+  });
 
 });
 
